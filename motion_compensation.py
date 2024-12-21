d48b3421f46e1efb95cea218cc72a7cdc3ba1823
Knowny <tomas.husar4401@gmail.com>
2024-12-21 02:16:59 +0100

    feat(): added placeholder motion_compensation

---
+++ motion_compensation.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+from tqdm import tqdm
+
+
+def compensate_motion(frames, flows):
+    """
+    TODO
+    """
+    #! PLACEHOLDER
+    stabilized_frames = 0
+
+    return stabilized_frames
