43e0df2ba8861754d94886af78acf93fd0fe1e28
Knowny <tomas.husar4401@gmail.com>
2024-12-23 16:45:32 +0100

    feat(): added --analyze_stabilization flag

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def parse_arguments():
     parser.add_argument("--method", help="Optical flow method to use (default: raft)", 
                         type=str, choices=["raft", "farneback", "deepflow"], default="raft")
     parser.add_argument("--visualize_flow", help="Visualize optical flow", action="store_true")
+    parser.add_argument("--analyze_stabilization", help="Analyze video stabilization", action="store_true")
     parser.add_argument("--save_flow", type=str, help="File to save the calculated flow.")
     parser.add_argument("--load_flow", type=str, help="File to load the precomputed flow.")
     return parser.parse_args()
@@ -73,13 +74,15 @@ def main(args):
     stabilized_frames = compensate_motion(frames, flows)
     save_video(stabilized_frames, f"output/stabilized_video_{args.method}.avi", fps, dimensions)
 
-    stabilized_flows = flow_method(stabilized_frames)
-    motion_check.plot_motion(flows, stabilized_flows)
+    #* STABILIZATION ANALYSIS
+    if args.analyze_stabilization:
+        stabilized_flows = flow_method(stabilized_frames)
+        motion_check.plot_motion(flows, stabilized_flows)
 
-    metrics = motion_check.evaluate_stabilization(frames, stabilized_frames, flows, stabilized_flows)
-    print("\nMotion Analysis Results:")
-    print(f"Original Mean Motion: {metrics['original_mean_motion']:.4f}")
-    print(f"Stabilized Mean Motion: {metrics['stabilized_mean_motion']:.4f}")
+        metrics = motion_check.evaluate_stabilization(frames, stabilized_frames, flows, stabilized_flows)
+        print("\nMotion Analysis Results:")
+        print(f"Original Mean Motion: {metrics['original_mean_motion']:.4f}")
+        print(f"Stabilized Mean Motion: {metrics['stabilized_mean_motion']:.4f}")
 
 if __name__ == '__main__':
     args = parse_arguments()
